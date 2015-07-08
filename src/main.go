15fb66f8e6abbe8b2a300567afd2025f7a4b2972
Mark LaPerriere <marklap@gmail.com>
2015-07-08 17:41:35 -0400

    split out template string to support multiple oses

--- src/main.go
+++ src/main.go
@@ -40,38 +40,6 @@ var (
     project_name string
 )
 
-const activateTmplSrc = `#!/bin/sh
-function deactivate() {
-  if [ -n "$_OLD_GOPJX_PATH" ]; then
-    export PATH="$_OLD_GOPJX_PATH"
-    unset _OLD_GOPJX_PATH
-  fi
-
-  if [ -n "$_OLD_GOPJX_GOPATH" ]; then
-    export GOPATH="$_OLD_GOPJX_GOPATH"
-    unset _OLD_GOPJX_GOPATH
-  fi
-
-  if [ -n "$_OLD_GOPJX_PS1" ]; then
-    export PS1="$_OLD_GOPJX_PS1"
-    unset _OLD_GOPJX_PS1
-  fi
-
-  if [ "$1" != "nondestructive" ]; then
-    unset -f deactivate
-  fi
-}
-
-deactivate nondestructive
-
-export _OLD_GOPJX_PATH="$PATH"
-export _OLD_GOPJX_GOPATH="$GOPATH"
-export _OLD_GOPJX_PS1="$PS1"
-export PS1="[go:{{.Name}}] $_OLD_GOPJX_PS1"
-export GOPATH={{.GoPath}}
-export PATH=$GOPATH/bin:$PATH
-`
-
 type activateTmplVars struct {
     Name   string
     GoPath string
