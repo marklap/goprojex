e2f03b38c72170352a7baaf7bee2c25f81e874f7
lapem001 <mark.a.laperriere@disney.com>
2017-12-01 13:53:43 -0800

    bump the version

--- main.go
+++ main.go
@@ -33,7 +33,7 @@ import (
 )
 
 // Version is the current semver of this project
-const Version = "0.1.1"
+const Version = "0.1.2"
 
 // GoProjexDir is the directory used for the activate script
 const GoProjexDir = ".gopjx"
